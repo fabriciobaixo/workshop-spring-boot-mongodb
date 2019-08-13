b85c07b34a8701e2951386c5b966f9c18130f6e5
Fabricio Baixo <fabriciobaixo@gmail.com>
2019-08-13 17:02:31 -0300

    Refereciando os posts do usuario

--- src/main/java/com/nelioalves/workshopmongo/config/Instantiation.java
+++ src/main/java/com/nelioalves/workshopmongo/config/Instantiation.java
@@ -40,9 +40,11 @@ public class Instantiation  implements CommandLineRunner{
 		
 		Post post1 = new Post(null, sdf.parse("13/08/2019"), "Partiu Viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
 		Post post2 = new Post(null, sdf.parse("14/08/2018"), "Bom dia", "Acordei Feliz Hoje", new AuthorDTO(maria));
-
-		
+	
 		postRepository.saveAll(Arrays.asList(post1,post2));
+		
+		maria.getPosts().addAll(Arrays.asList(post1,post2));
+		userRepository.save(maria);
 	}
 
 }
